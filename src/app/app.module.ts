a827d4c0bc7175417f2ba50bf2fccdd36dd5926c
unknown <darshansivaram07@gmail.com>
2020-12-15 00:14:26 +0530

    Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
-
+import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+import { CardComponent } from './card/card.component'
+ 
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardFormComponent,
+    InputComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
